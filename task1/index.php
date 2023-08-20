da50164a86dda46701a5d9b73945b39fdaeced84
samad4509 <samad4509@gmail.com>
2023-08-20 21:56:15 +0600

    first commit update

--- task1/index.php
+++ task1/index.php
@@ -1,94 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-interface UserInterface {
-    public function getUsername();
-    public function getPassword();
-}
+</head>
+<body>
+    <form action="task2.php" method="post">
 
-class User implements UserInterface {
-    private $username;
-    private $password;
+       <div class="container">
+            <div class="row mb-4">
+                    <label for="" class="col-md-3">User Name</label>
+                    <div class="col-md-9">
+                        <input type="text" name="username" class="form-controll"/>
+                    </div>
+            </div>
+            <div class="row mb-4">
+                <label for="" class="col-md-3">Password</label></label>
+                <div class="col-md-9">
+                    <input type="text" name="password" class="form-controll"/>
+                </div>
+            </div>
+            <div class="row mb-4">
+                <label for="" class="col-md-3"></label>
+                <div class="col-md-9">
+                    <input type="submit" name="submit" value="Submit">
+                </div>
+            </div>
+       </div>
+     </form>
 
-    public function __construct($username, $password) {
-        $this->username = $username;
-        $this->password = $password;
-    }
-
-    public function getUsername() {
-        return $this->username;
-    }
-
-    public function getPassword() {
-        return $this->password;
-    }
-}
-
-
-interface AuthenticatorInterface {
-    public function authenticate(UserInterface $user, $password);
-}
-
-class SimpleAuthenticator implements AuthenticatorInterface {
-    private $validUsers = [];
-
-    public function addUser(UserInterface $user) {
-        $this->validUsers[$user->getUsername()] = $user->getPassword();
-    }
-
-    public function authenticate(UserInterface $user, $password) {
-        $username = $user->getUsername();
-        if (isset($this->validUsers[$username]) && $this->validUsers[$username] === $password) {
-            return true;
-        }
-        return false;
-    }
-}
-
-function createUserInput() {
-    echo "Create a new user account\n";
-    echo "Enter username: ";
-    $username = readline();
-    echo "Enter password: ";
-    $password = readline();
-    return new User($username, $password);
-}
-
-function loginUserInput() {
-    echo "User Login\n";
-    echo "Enter username: ";
-    $username = readline();
-    echo "Enter password: ";
-    $password = readline();
-    return new User($username, $password);
-}
-
-$authenticator = new SimpleAuthenticator();
-
-while (true) {
-    echo "1. Create User\n2. Login\n3. Exit\n";
-    echo "Enter your choice: ";
-    $choice = readline();
-
-    switch ($choice) {
-        case '1':
-            $user = createUserInput();
-            $authenticator->addUser($user);
-            echo "User created successfully.\n";
-            break;
-        case '2':
-            $user = loginUserInput();
-            if ($authenticator->authenticate($user, $user->getPassword())) {
-                echo "Login successful. Welcome, " . $user->getUsername() . "!\n";
-            } else {
-                echo "Login failed. Invalid username or password.\n";
-            }
-            break;
-        case '3':
-            echo "Goodbye!\n";
-            exit;
-        default:
-            echo "Invalid choice. Please try again.\n";
-    }
-}
-
-?>
+</body>
+</html>
